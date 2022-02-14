2edf01e070917eaa97c245cae060713d0ce18a23
mrmtsu <81202muramatsu@gmail.com>
2022-02-15 05:34:10 +0900

    ✨ create graphql server

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './components/posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
